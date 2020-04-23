99053bcf0b41df2da98799690ef82109059070e4
ahmed magdy <h.dabour25@yahoo.com>
2020-04-23 10:36:34 +0200

    update css

--- resources/views/user/master.blade.php
+++ resources/views/user/master.blade.php
@@ -10,15 +10,46 @@
         <link rel="stylesheet" href="{{asset('/css/bootstrap.min.css')}}">
         <link rel="stylesheet" href="{{asset('/css/bootstrap-grid.css')}}">
 
+        <!--Css Files -->
+        <link rel="stylesheet" href="{{asset('/css/style.css')}}">
+
+        <!--Fonts -->
+        <style>
+            @font-face {
+                font-family: Lovelo-Black;
+                src: url({{asset('/fonts/Lovelo-Black.otf')}});
+            }
+        </style>
+
     </head>
     <body>
         <header>
-
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-xs-2">
+                        <a href="/">
+                            <img src="{{asset('/imgs/tiny-logo.png')}}" class="header-logo">
+                            <img src="{{asset('/imgs/gems.png')}}" class="header-gems">
+                        </a>
+                    </div>
+                    <div class="col-sm-3 language-div">
+                        <a href="/lang/en" class="lang">
+                            English
+                        </a>|
+                        <a href="/lang/ar" class="lang">
+                            Arabic
+                        </a>
+                    </div>
+                    <div class="col-sm-4 main-word">
+                        GEMS MEGA STORE
+                    </div>
+                </div>
+            </div>
         </header>
     @yield('content')
 
     </body>
-    <!-- JQuery & Bootstrap -->
+    <!-- Bootstrap -->
     <script src="{{asset('/js/bootstrap.bundle.min.js')}}"></script>
     <script src="{{asset('/js/bootstrap.min.js')}}"></script>
 </html>
